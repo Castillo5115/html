44a517534e405e2399a6d88ef9547c2b8cf8f705
Castillo5115 <adriancastillo51.15@gmail.com>
2022-11-09 17:51:12 +0100

    Creacion de juegos

--- rectangulo.py
+++ rectangulo.py
@@ -3,5 +3,16 @@ import sys
 pygame.init()
 
 PANTALLA = pygame.display.set_mode((500,400))
-pygame.display.set_caption('Mi primer juego')
+BLANCO = (255, 255, 255);
+ROJO = (255, 0, 0);
 
+pygame.display.set_caption('Mi primer juego')
+PANTALLA.fill(BLANCO);
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    pygame.draw.rect(PANTALLA, ROJO, (100,50,100,50));
