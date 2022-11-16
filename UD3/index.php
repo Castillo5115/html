3945de3c295ff8affb4c0cf4371022d0f59cabd0
Castillo5115 <adriancastillo51.15@gmail.com>
2022-11-16 17:50:39 +0100

    cambio dee nombres

--- UD3/index.php
+++ UD3/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilosPruebas.css">
+    <link rel="stylesheet" href="estilos.css">
     <title>Document</title>
 </head>
 <body>
@@ -43,9 +43,9 @@
                     <a href="" class="ficha">Ver Ficha</a>
                 </div>
             </div>
-
-        </div>
-        
+            <?php
+            ?>
+        </div>        
     </div>
 </body>
 </html>
