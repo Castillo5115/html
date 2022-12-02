4f6158f939d4b4a97850e6044c1d35792da3d1ef
Castillo5115 <adriancastillo51.15@gmail.com>
2022-12-02 19:05:46 +0100

    cambios importantes crear variables con el init

--- UD3/Peliculas.php
+++ UD3/Peliculas.php
@@ -22,6 +22,9 @@
 
                     
                     function init($id, $titulo, $duracionMin, $votos, $id_categoria){
+
+
+                        
                         $this->id = $id;
                         $this->titulo = $titulo;
                         $this->duracionMin = $duracionMin;
@@ -29,10 +32,10 @@
                         $this->id_categoria = $id_categoria;
                     }
                     // Conexion a mysql
-                    function dameDatos($id_categoria){
+                    function dameDatos( ){
                         $conexion = mysqli_connect('localhost', 'root', '12345');
                         mysqli_select_db($conexion, 'aficine');
-                        $consulta = "SELECT * FROM Pelicula WHERE id_categoria = '$id_categoria'";
+                        $consulta = "SELECT * FROM Pelicula";
                         $resultado = mysqli_query($conexion, $consulta);
                         if (!$resultado) {
                             $mensaje = 'Consulta invalida: ' . mysqli_error($conexion) . "\n";
@@ -41,25 +44,36 @@
                         }else{
                             $i = 0;
                             $arrayPeliculas = array();
-                            while ($conatador < $registro = mysqli_fetch_assoc($resultado)) {
 
+                            while ($registro = mysqli_fetch_assoc($resultado)) {
+                                echo "Hola";
                                 $p = new Pelicula();
-                                $arrayPeliculas[$i] = $p -> init($registro['id'], $registro['titulo'], $registro['duracionMin'], $registro['votos'], $registro['id_categoria']);
-                                        $i++;
+                                echo $registro['titulo'];
+
+                                $titulo = $registro['titulo'];
+
+                                $p->init('$id', $titulo, '$duracionMin', '$votos', '$id_categoria');
+
+                                var_dump($p);
+                                // $arrayPeliculas[$i] = $p->init($registro['titulo']);
+                                $i++;
+                            
                             }
-                            return $arrayPeliculas;
+                            var_dump($arrayPeliculas);
                         }
                     }
 
                     function pintarPeliculas(){
                         
                     }
+
+
                 }
                 
                 
                 $p = new Pelicula;
-                $datos = $p -> dameDatos("terror");
                 $p -> pintarPeliculas();
+                $p -> dameDatos();
             ?>
         </div>        
     </div>
