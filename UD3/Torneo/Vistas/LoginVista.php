66524ecac170c36528843f1642bfddb6edc0184b
Castillo5115 <adriancastillo51.15@gmail.com>
2023-01-31 20:57:34 +0100

    Login funcional 2023-01-31

--- UD3/Torneo/Vistas/LoginVista.php
+++ UD3/Torneo/Vistas/LoginVista.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST")
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Torneo Ping Pong</title>
-    <!-- <link rel="stylesheet" href="../css/login.css"> -->
+    <link rel="stylesheet" href="../css/login.css">
 </head>
 
 <body>
