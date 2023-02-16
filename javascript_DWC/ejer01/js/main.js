e723756c43921b05bc106e9a58b4a1ebe5be3965
Castillo5115 <102173631+Castillo5115@users.noreply.github.com>
2023-02-16 20:25:09 +0100

    depurando codigo 2023-02-16

--- javascript_DWC/ejer01/js/main.js
+++ javascript_DWC/ejer01/js/main.js
@@ -5,67 +5,17 @@ window.onload = function(){
 
 
   function dame_ciudades(){
-    dato = nodoText.value
-    if (dato.length == 0) {
-      document.getElementById("datos").innerHTML = "";
-      return;
-    } else {
-      const xmlhttp = new XMLHttpRequest();
-      xmlhttp.onload = function() {
-        document.getElementById("datos").innerHTML = this.responseText;
+      dato = nodoText.value
+      if (dato.length == 0) {
+        document.getElementById("datos").innerHTML = "";
+        return;
+      } else {
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function() {
+          document.getElementById("datos").innerHTML = this.responseText;
+        }
+      xmlhttp.open("GET", "php/db.php?dato=" + dato);
+      xmlhttp.send();
       }
-    xmlhttp.open("GET", "php/db.php?dato=" + dato);
-    xmlhttp.send();
-    }
-}
-
-}
-
-
-function dame_ciudades(dato){
-  if (dato.length == 0) {
-    document.getElementById("datos").innerHTML = "";
-    return;
-  } else {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function() {
-      document.getElementById("datos").innerHTML = this.responseText;
-    }
-  xmlhttp.open("GET", "php/db.php?dato=" + dato);
-  xmlhttp.send();
   }
 }
-
-
-// window.onload = function(){
-//   let nodoText = document.getElementById("ciudad");
-//   console.log(document.getElementById("ciudad"));
-//   nodoText.addEventListener("keyup",mandarLetra);
-  
-  
-  
-//   function mandarLetra(){
-//      letra = nodoText.value
-//       if (letra.length == 0) {
-//           document.getElementById("sugerencias").innerHTML = "";
-          
-//       }else {
-         
-//           const xmlhttp = new XMLHttpRequest();
-//           xmlhttp.onreadystatechange = function() {
-             
-             
-                  
-//                   document.getElementById("sugerencias").innerHTML = this.responseText;
-              
-            
-            
-//           }
-  
-//           xmlhttp.open("GET", "../php/ciudades.php?letra=" + letra);
-//           xmlhttp.send();
-       
-//         }
-       
-//   }
-//   }
