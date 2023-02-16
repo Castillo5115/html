70f4bbad3bec7928e4647362141658ff75eb4d55
Castillo5115 <adriancastillo51.15@gmail.com>
2023-02-16 16:47:37 +0100

    Ejer01 funciona con evento en js

--- javascript_DWC/ejer01/js/main.js
+++ javascript_DWC/ejer01/js/main.js
@@ -1,9 +1,28 @@
-window.onload = function() {
-  let letra = document.getElementById('letra');
-  letra.addEventListener('keyup', dame_datos);
+window.onload = function(){
+  let nodoText = document.getElementById("letra");
+  console.log(nodoText);
+  nodoText.addEventListener("keyup", dame_ciudades);
+
+
+  function dame_ciudades(){
+    dato = nodoText.value
+    if (dato.length == 0) {
+      document.getElementById("datos").innerHTML = "";
+      return;
+    } else {
+      const xmlhttp = new XMLHttpRequest();
+      xmlhttp.onload = function() {
+        document.getElementById("datos").innerHTML = this.responseText;
+      }
+    xmlhttp.open("GET", "php/db.php?dato=" + dato);
+    xmlhttp.send();
+    }
 }
 
-function dame_datos(dato){
+}
+
+
+function dame_ciudades(dato){
   if (dato.length == 0) {
     document.getElementById("datos").innerHTML = "";
     return;
@@ -12,7 +31,41 @@ function dame_datos(dato){
     xmlhttp.onload = function() {
       document.getElementById("datos").innerHTML = this.responseText;
     }
-  xmlhttp.open("GET", "php/db.php?str=" + dato);
+  xmlhttp.open("GET", "php/db.php?dato=" + dato);
   xmlhttp.send();
   }
 }
+
+
+// window.onload = function(){
+//   let nodoText = document.getElementById("ciudad");
+//   console.log(document.getElementById("ciudad"));
+//   nodoText.addEventListener("keyup",mandarLetra);
+  
+  
+  
+//   function mandarLetra(){
+//      letra = nodoText.value
+//       if (letra.length == 0) {
+//           document.getElementById("sugerencias").innerHTML = "";
+          
+//       }else {
+         
+//           const xmlhttp = new XMLHttpRequest();
+//           xmlhttp.onreadystatechange = function() {
+             
+             
+                  
+//                   document.getElementById("sugerencias").innerHTML = this.responseText;
+              
+            
+            
+//           }
+  
+//           xmlhttp.open("GET", "../php/ciudades.php?letra=" + letra);
+//           xmlhttp.send();
+       
+//         }
+       
+//   }
+//   }
--- javascript_DWC/ejer01/php/db.php
+++ javascript_DWC/ejer01/php/db.php
@@ -2,8 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('html_errors', 1);
 
-$letra = $_REQUEST['str'];
-$comparador = strtoupper($letra);
+$letra = $_REQUEST["dato"];
 
 $conexion = mysqli_connect('localhost', 'root', '12345');
 mysqli_select_db($conexion, 'world');
